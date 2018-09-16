36d63e538dac873ad1bdf9c27ce18ca6eebf0eef
Serhii Melnyk <serj.melnyk@gmail.com>
2018-09-16 18:46:23 +0300

    1.Add Composer ,Feed , Post

---
+++ source/components/Composer/index.js
@@ -0,0 +1,18 @@
+//Core
+import React, { Component } from 'react';
+//Instruments
+import avatar from 'theme/assets/lisa';
+
+export default class Composer extends Component {
+    render () {
+        return (
+            <section>
+                <img src = { avatar } />
+                <form>
+                    <textarea placeholder = { 'Whats on your mind , Lisa' } />
+                    <input type = 'submit' value = 'Post' />
+                </form>
+            </section>
+        );
+    }
+}
