a85e33945be1b34837a814a91de94ef8a8bff0ca
Serhii Melnyk <serj.melnyk@gmail.com>
2018-09-25 17:32:03 +0300

    minor fix code-style

--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -11,14 +11,15 @@ import Styles from './styles.m.css';
 export default class Post extends Component {
     render () {
         return (
-            <Consumer>{(context) => (
-                <section className = { Styles.post }>
-                    <img src = { context.avatar } />
-                    <a>{` ${context.currentUserFirstName} ${context.currentUserLastName} `}</a>
-                    <time>{moment().format('MMMM D h:mm:ss a')}</time>
-                    <p>Howdy!</p>
-                </section>
-            )}
+            <Consumer>
+                {(context) => (
+                    <section className = { Styles.post }>
+                        <img src = { context.avatar } />
+                        <a>{` ${context.currentUserFirstName} ${context.currentUserLastName} `}</a>
+                        <time>{moment().format('MMMM D h:mm:ss a')}</time>
+                        <p>Howdy!</p>
+                    </section>
+                )}
             </Consumer>
         );
     }
