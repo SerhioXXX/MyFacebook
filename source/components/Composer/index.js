47cbd40c8e901845958463297563cedaf72b99c6
Serhii Melnyk <serj.melnyk@gmail.com>
2018-09-25 13:40:08 +0300

    add to one-way data flow (props) in project

--- source/components/Composer/index.js
+++ source/components/Composer/index.js
@@ -1,16 +1,20 @@
 //Core
 import React, { Component } from 'react';
 //Instruments
-import avatar from 'theme/assets/lisa';
 import Styles from './styles.m.css';
 
 export default class Composer extends Component {
     render () {
+        const {
+            currentUserFirstName,
+            avatar,
+        } = this.props;
+
         return (
             <section className = { Styles.composer }>
                 <img src = { avatar } />
                 <form>
-                    <textarea placeholder = { 'Whats on your mind , Lisa' } />
+                    <textarea placeholder = { `Whats on your mind , ${currentUserFirstName} ?` } />
                     <input type = 'submit' value = 'Post' />
                 </form>
             </section>
--- source/components/StatusBar/index.js
+++ source/components/StatusBar/index.js
@@ -3,9 +3,25 @@ import React, { Component } from 'react';
 
 //Instruments
 import Styles from './styles.m.css';
+//Instruments
 
 export default class StatusBar extends Component {
     render () {
-        return <section className = { Styles.statusBar } />;
+        const {
+            currentUserFirstName,
+            currentUserLastName,
+            avatar,
+        } = this.props;
+
+        return (
+            <section className = { Styles.statusBar }>
+                <button>
+                    <img src = { avatar } />
+                    <span>{currentUserFirstName}</span>
+                &nbsp;
+                    <span>{currentUserLastName}</span>
+                </button>
+            </section>
+        );
     }
 }
