2df03ecaf1b074fc11e45aa5e6cfcec5efdf77f3
Serhii Melnyk <serj.melnyk@gmail.com>
2018-10-24 00:30:21 +0300

    add unit test for Composer component

--- source/components/Composer/index.js
+++ source/components/Composer/index.js
@@ -1,6 +1,6 @@
 //Core
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import { func, string } from 'prop-types';
 
 //Components
 import { withProfile } from 'components/HOC/withProfile';
@@ -8,10 +8,11 @@ import { withProfile } from 'components/HOC/withProfile';
 //Instruments
 import Styles from './styles.m.css';
 
-@withProfile
-export default class Composer extends Component {
+export class Composer extends Component {
     static propTypes = {
-        _createPost: PropTypes.func.isRequired,
+        _createPost:          func.isRequired,
+        avatar:               string.isRequired,
+        currentUserFirstName: string.isRequired,
     };
 
     state = {
@@ -30,7 +31,6 @@ export default class Composer extends Component {
     }
 
     _submitComment = () => {
-        event.preventDefault();
 
         const { comment } = this.state;
 
@@ -75,3 +75,5 @@ export default class Composer extends Component {
         );
     }
 }
+
+export default withProfile(Composer);
