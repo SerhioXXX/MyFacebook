7d40613c5361544d653096ece4e78e308ba14747
Serhii Melnyk <serj.melnyk@gmail.com>
2018-10-23 16:16:07 +0300

    add unit tests for getFullApiUrl func

--- source/instruments/index.test.js
+++ source/instruments/index.test.js
@@ -1,5 +1,5 @@
 //Core
-import { sum, delay, getUniqueID } from './';
+import { sum, delay, getUniqueID, getFullApiUrl } from './';
 
 jest.setTimeout(10000);
 
@@ -37,6 +37,22 @@ describe('instrumentsS:', () => {
         expect(typeof getUniqueID()).toBe('string');
         expect(getUniqueID(5)).toHaveLength(5);
         expect(getUniqueID(13)).toHaveLength(13);
+    });
+
+    test('getFullApiUrl should be a function', () => {
+        expect(getFullApiUrl).toBeInstanceOf(Function);
+    });
+
+    test('getFullApiUrl function should throw, when called with type number of first argument', () => {
+        expect(() => getFullApiUrl('api', 2)).toThrow();
+    });
+
+    test('getFullApiUrl function should throw, when called with type number of second argument', () => {
+        expect(() => getFullApiUrl(3, 'GROUP_ID')).toThrow();
+    });
+
+    test('getFullApiUrl must return Url', () => {
+        expect(getFullApiUrl('api', 'GROUP_ID')).toBe('api/GROUP_ID');
     });
 
 });
