0114ae919910de60c955497c953f29de7860fb62
Serhii Melnyk <serj.melnyk@gmail.com>
2018-11-02 17:20:45 +0200

    add BrowserRouter to App

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -9,7 +9,6 @@ import { fromTo } from 'gsap';
 
 //Components
 import { withProfile } from 'components/HOC/withProfile';
-import StatusBar from 'components/StatusBar';
 import Composer from 'components/Composer';
 import Post from 'components/Post';
 import Catcher from 'components/Catcher';
@@ -213,7 +212,6 @@ export default class Feed extends Component {
         return (
             <section className = { Styles.feed }>
                 <Spinner isSpinning = { isPostFetching } />
-                <StatusBar />
                 <Transition
                     appear
                     in
