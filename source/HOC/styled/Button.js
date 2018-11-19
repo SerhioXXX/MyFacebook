7ebeb8e6073b0712a559e43d90ff83349a4118fe
Serhii Melnyk <serj.melnyk@gmail.com>
2018-11-19 15:42:36 +0200

    change css

--- source/HOC/styled/Button.js
+++ source/HOC/styled/Button.js
@@ -2,7 +2,7 @@
 import styled from 'styled-components';
 
 export const Button = styled.button`
-    border-radius: 3px;
+    border-radius: 4px;
     padding: 10px 7px;
     width: 200px;
     background: transparent;
